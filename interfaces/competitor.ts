eb21943849943b61f652e87a21b2ff1bce35e737
Marta <marrta98@wp.pl>
2023-01-28 13:03:26 +0100

    Added node modules and created data models

---
+++ interfaces/competitor.ts
@@ -0,0 +1,9 @@
+export interface ICompetitor {
+	id: string
+	name: string
+	country: string
+	country_code: string
+	abbreviation: string
+	qualifier: string
+	gender: string
+}
---
+++ interfaces/venue.ts
@@ -0,0 +1,9 @@
+export interface IVenue {
+	id: string
+	name: string
+	capacity: number
+	city_name: string
+	country_name: string
+	map_cooridinates: string
+	country_code: string
+}
